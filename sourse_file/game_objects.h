c068cc824cd86767f9bd374b952da8dacaa2926a
XiyaoWang <wangxiyao2003@gmail.com>
2023-05-21 19:07:27 +0800

    update committed

---
+++ sourse_file/game_objects.h
@@ -0,0 +1,29 @@
+typedef struct
+{
+    int x;
+    int y;
+    int width;
+    int height;
+} Player;
+
+// Define the structure for the train
+typedef struct
+{
+    int x;
+    int y;
+    int width;
+    int height;
+    int color;
+    int track;
+    int speed;
+} Train;
+
+// Define the structure for the coin
+typedef struct
+{
+    int x;
+    int y;
+    int width;
+    int height;
+    int color;
+} Coin;
